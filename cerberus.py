da8a1901aefc23f2803d291151f5435fa2ef59eb
Mohamed <mrmoha94@gmail.com>
2019-11-27 14:36:15 +0100

    modification suggérée pendant le spectacle

--- cerberus.py
+++ cerberus.py
@@ -4,27 +4,24 @@ from random import *
 from string import *
 from csv import *
 
-print("Entrez le nom/url :")
-url = input()
-print("\nEntrez votre identifiant :")
-identifiant = input()
-print("\nChoisir la taille du mdp :")
+url = input("Entrez le nom/url : ")
+identifiant = input("Entrez votre identifiant : ")
+taille = int(input("Choisir la taille du mdp : "))
 
 caracteres = []
-taille = input()
-conv = int(taille)
 mdp = ""
 compteur = 0
 
 for i in range(33,127):
 	caracteres.append(chr(i))
 
-while compteur < conv:
+while compteur < taille:
 	lettre = choice(caracteres)
 	mdp += lettre
 	compteur += 1
 
 print("\nVotre mdp est :",mdp,"\n")
 
-with open ("liste_mdp", "w") as liste_mdp:
+with open ("liste_mdp", "a") as liste_mdp:
 	liste_mdp.write(url + "\n" + identifiant + "\n" + mdp)
+#separer par des virgules et ajouter csv
