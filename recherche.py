18ab231400aadd0f7150424dd14dd9f9c8d6f86e
lydiasan <lyd.sanaa@gmail.com>
2019-11-28 10:19:18 +0100

    test de conflit -lyd

--- recherche.py
+++ recherche.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-chaine = input() 
+chaine = input("Choisissez le compte à rechercher : ") 
 with open("liste_mdp","r") as fichier:
 	for ligne in fichier:
 		if chaine in ligne: 
