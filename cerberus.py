52f15bd86dc0308285fdcbbe43a66915dd49d1b6
lydiasan <lyd.sanaa@gmail.com>
2019-11-28 14:34:32 +0100

    renommage fichiers

--- cerberus.py
+++ cerberus.py
@@ -1,26 +1,4 @@
 #!/usr/bin/python3
 
-from random import *
-from string import *
-#from csv import *
-
-url = input("Entrez le nom/url : ")
-identifiant = input("Entrez votre identifiant : ")
-taille = int(input("Choisir la taille du mdp : "))
-
-caracteres = []
-mdp = ""
-compteur = 0
-
-for i in range(33,127):
-	caracteres.append(chr(i))
-
-while compteur < taille:
-	lettre = choice(caracteres)
-	mdp += lettre
-	compteur += 1
-
-print("\nVotre mdp est :",mdp,"\n")
-
-with open ("liste_mdp", "a") as liste_mdp:
-	liste_mdp.write(url + " , " + identifiant + " , " + mdp + "\n")
+import cerberus
+import recherche
--- main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-
-import cerberus
-import recherche
